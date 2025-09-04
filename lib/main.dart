1f4b61e76fd461aa9295a5225487399ba95f6746
RashadZA <97411218+RashadZA@users.noreply.github.com>
2025-09-04 23:41:13 +0600

    Installed FIrebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:fashion_shoping/core/routes/app_pages.dart';
 import 'package:fashion_shoping/core/utils/design_utils.dart';
+import 'package:fashion_shoping/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
